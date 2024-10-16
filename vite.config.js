b759fa27c5c58fbc9a99f18fb22932421446cb63
troy1eighty2 <troytran000@gmail.com>
2024-10-16 11:51:31 -0500

    new

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  historyApiFallback: true
+  server: {
+    historyApiFallback: true
+  }
 })
