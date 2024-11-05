4e48f6a1a7e2f12b61633f45ff5092bdc5e4a66f
troy1eighty2 <troytran000@gmail.com>
2024-11-05 13:30:15 -0600

    video

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -34,7 +34,7 @@ function Content() {
             case "a":
               return <a href={item.title} target="_blank">{item.link}</a>;
             case "iframe":
-              return <iframe width="300px" height="169px" src={item.title} allowfullscreen></iframe>
+              return <iframe width="560" height="315" src="https://www.youtube.com/embed/X6AR2RMB5tE?si=RLJECLD9MX8zlAfZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             default:
               return <Tag key={index}>{item.title}</Tag>
           };
